b3ecf5c1e0ef8195ded26e9b424da1fc8921feea
Dredayduncan <50704111+Dredayduncan@users.noreply.github.com>
2020-01-24 08:21:35 +0000

    Update Product.java

--- Essentials_Shops/Product.java
+++ Essentials_Shops/Product.java
@@ -1,10 +1,20 @@
+/**
+*
+*/
 package Essentials_Shops;
 
 public class Product {
+    //instance variables are the properties of each product
     String item;
     double price;
     int quantity;
 
+    /**
+    *@param product is the name of the product
+    *@param amount is the quantity of the product
+    *@param cost is the price of one quantity of the product
+    *@return The constructor does not return anything
+    */
     public Product(String product, int amount, double cost) {
         item = product;
         price = cost;
