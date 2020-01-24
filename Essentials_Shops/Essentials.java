75a02f053eec3c3dca00e486b0336816b596e6f7
Dredayduncan <50704111+Dredayduncan@users.noreply.github.com>
2020-01-24 08:35:00 +0000

    comments have been added

--- Essentials_Shops/Essentials.java
+++ Essentials_Shops/Essentials.java
@@ -1,5 +1,9 @@
+/**
+*
+*/
 package Essentials_Shops;
 
+// Import the necessary modules for file manipulation
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -7,14 +11,25 @@ import java.io.FileOutputStream;
 import java.util.Scanner;
 
 public class Essentials {
+    
     PrintWriter printer = new PrintWriter(new FileOutputStream("essentials_stock.txt", true));
-    PrintWriter safe = new PrintWriter(new FileOutputStream("backup_essentials_stock.txt", true));
-
+    
+    /**
+    *The constructor sets the headers in the file 
+    *@return The constructor does not return anything
+    */
     public Essentials() throws FileNotFoundException {
         printer.println("Item" + " " + "Quantity" + " " + "Price");
         printer.close();
     }
-
+    
+    /**
+    *This method enables the user to add products to stock
+    *@param itemName is the name of the product 
+    *@param quant is the quantity of the product being added
+    *@param price is the price of a single quantity of the product being added
+    *@return The constructor does not return anything
+    */
     public void addTostock(String itemName, int quant, double price) throws FileNotFoundException {
         PrintWriter printer = new PrintWriter(new FileOutputStream("essentials_stock.txt", true));
         Product item = new Product(itemName, quant, price);
@@ -23,7 +38,11 @@ public class Essentials {
         System.out.println("Inventory has been updated");
     }
 
+    /**
+    * The method reads the file that has the stock info and created a duplicate
+    */
     public void backup() throws FileNotFoundException {
+        PrintWriter safe = new PrintWriter(new FileOutputStream("backup_essentials_stock.txt", true));
         File initFile = new File("essentials_stock.txt");
         Scanner reader = new Scanner(initFile);
         while (reader.hasNext())
@@ -33,6 +52,9 @@ public class Essentials {
 
     }
 
+    /**
+    * This method displays the data in the original file containing the stock information
+    */
     public void display() throws FileNotFoundException {
         File initFile = new File("essentials_stock.txt");
         Scanner reader = new Scanner(initFile);
